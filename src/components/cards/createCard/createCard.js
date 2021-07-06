903f5509823703842d0ff0af9b5d148221f12978
Craig Satchell <satchdev@gmail.com>
2021-07-05 21:01:59 -0400

    add CreateCard

--- src/components/cards/createCard/createCard.js
+++ src/components/cards/createCard/createCard.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Card, CardContent } from '@material-ui/core';
 import './createCard.css';
 
-const CreateCard = () => {
+const useStyles = makeStyles((theme) => ({
+   card: {
+      padding: theme.spacing(0),
+      margin: 0,
+      color: 'var(--pri-text-color)',
+      backgroundColor: '#999999',
+   },
+   cardContent: {
+      minHeight: '300px',
+
+   },
+   cardActions: {
+      display: 'flex',
+      marginBottom: theme.spacing(2),
+      marginRight: theme.spacing(2),
+      justifyContent: 'space-between',
+   }
+}));
+const CreateCard = (props) => {
+   const classes = useStyles();
    return (
       <div className='create-card'>
-         <h3>Create Card</h3>
+         <Card className={classes.card}>
+            <CardContent className={classes.cardContent}>
+               <Typography gutterBottom variant="h5" component="h2">
+                  <div>FlashCard - {props.decks[props.selectedDeck].title}</div>
+               </Typography>
+               <Typography variant="h4" >
+                  <form action="">
+                     <input type="text" name="term" value='' placeholder='Term' />
+                     <input type="text" name="definition" value='' placeholder='Definition' />
+                     <input type="submit" name='save' value='Save' />
+                  </form>
+               </Typography>
+            </CardContent>
+         </Card>
       </div>
    )
 }
